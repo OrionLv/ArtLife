0404f735adb2ea4b35c3d8869df4eba287265530
lxb <lxb@lxbdeMacBook-Air.local>
2017-02-17 17:10:22 +0800

    Initial Commit

---
+++ ArtLife/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ArtLife
+//
+//  Created by lxb on 2017/2/17.
+//  Copyright © 2017年 lxb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
